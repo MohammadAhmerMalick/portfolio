7e1f05e4acf1f7f4434d7b691d73b9f4b467ff08
unknown <mohammadahmermalick@gmail.com>
2022-12-11 20:59:56 +0500

    feat: single portfolio item component initiated

---
+++ pages/portfolio/[id].tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react'
+import { GetServerSideProps } from 'next'
+
+import Contact from '@/components/sections/contact/Contact'
+import SectionHeading from '@/components/common/section/sectionHeading/SectionHeading'
+import SectionContainer from '@/components/common/section/sectionContainer/SectionContainer'
+
+interface SinglePortfolio {
+  id: string
+}
+
+const SinglePortfolio: FC<SinglePortfolio> = ({ id }) => {
+  return (
+    <>
+      <SectionContainer>
+        <SectionHeading
+          primary="Bizzworld Communicatinos"
+          paragraph="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis tempora similique voluptate nesciunt excepturi ipsam debitis doloremque ab odit maiores."
+        />
+        <article>a</article>
+      </SectionContainer>
+
+      <Contact />
+    </>
+  )
+}
+
+export default SinglePortfolio
+
+export const getServerSideProps: GetServerSideProps = async ({ params }) => ({
+  props: { id: params?.id },
+})

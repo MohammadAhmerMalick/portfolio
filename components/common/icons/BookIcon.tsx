d46895f83737f345b31f93fd38206375505543df
unknown <mohammadahmermalick@gmail.com>
2022-12-22 16:07:54 +0500

    feat: contact form ui updated

---
+++ components/common/icons/BookIcon.tsx
@@ -0,0 +1,18 @@
+// BiBook
+function BookIcon() {
+  return (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth={0}
+      viewBox="0 0 24 24"
+      height="1em"
+      width="1em"
+    >
+      <path d="M6,22h15v-2H6.012C5.55,19.988,5,19.805,5,19s0.55-0.988,1.012-1H19h1h1v-1v-2V4c0-1.103-0.897-2-2-2H6 C4.794,2,3,2.799,3,5v3v6v3v2C3,21.201,4.794,22,6,22z M5,8V5c0-0.805,0.55-0.988,1-1h13v11v1H5v-2V8z" />
+      <path d="M8 6H17V8H8z" />
+    </svg>
+  )
+}
+
+export default BookIcon

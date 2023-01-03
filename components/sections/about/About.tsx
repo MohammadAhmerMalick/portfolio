a04c7099de165668752c1eec59b8944a497cae4a
unknown <mohammadahmermalick@gmail.com>
2023-01-03 09:51:58 -0800

    feat: resume added in about section

--- components/sections/about/About.tsx
+++ components/sections/about/About.tsx
@@ -12,31 +12,47 @@ const About = () => {
     <SectionContainer>
       <SectionHeading primary="About" secondary="Me">
         <div className={S.about}>
-          <p>
-            I am a Computer Scientist with more than three years of field
-            experience as a Web Developer. I have developed analytical skills
-            with expertise in handling teams along with other challenges. I am
-            Specialize in developing responsive, search engine optimized, speed
-            optimized web applications and websites and have extensive
-            experience in frontend and can handle backend on multiple
-            programming languages, also capable of handling a variety of
-            graphic-based tasks as well.
-          </p>
-          <p>
-            I am passionate about growing businesses profitably. I aspire to
-            create value with my work, both personally and professionally.
-          </p>
-          <p>
-            Currently located in UAE till February-20-2023 and you can contact
-            me through{' '}
+          <div className={S.content}>
+            <p>
+              I am a Computer Scientist with more than three years of field
+              experience as a Web Developer. I have developed analytical skills
+              with expertise in handling teams along with other challenges. I am
+              Specialize in developing responsive, search engine optimized,
+              speed optimized web applications and websites and have extensive
+              experience in frontend and can handle backend on multiple
+              programming languages, also capable of handling a variety of
+              graphic-based tasks as well.
+            </p>
+            <p>
+              I am passionate about growing businesses profitably. I aspire to
+              create value with my work, both personally and professionally.
+            </p>
+            <p>
+              Currently located in UAE till February-20-2023 and you can contact
+              me through{' '}
+              <Link
+                target="_blank"
+                className={S.link}
+                href={`tel:+${ContactEnum.PakistanContactNumber}`}
+              >
+                +{ContactEnum.UAEContactNumber}
+              </Link>
+            </p>
+          </div>
+          <div className={S.resume}>
+            <img
+              className={S.resumeQRCode}
+              src="./images/resume-qr-code.svg"
+              alt="mohammadahmermalick.com/resume/Mohammad%20Ahmer%20Malick%20CV.pdf"
+            />
             <Link
               target="_blank"
               className={S.link}
-              href={`tel:+${ContactEnum.PakistanContactNumber}`}
+              href="/resume/Mohammad%20Ahmer%20Malick%20CV.pdf"
             >
-              +{ContactEnum.UAEContactNumber}
+              Mohammad Ahmer Malick Resume
             </Link>
-          </p>
+          </div>
         </div>
       </SectionHeading>
     </SectionContainer>
